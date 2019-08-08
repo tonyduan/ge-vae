58e274a6597f36cce1f715a2c17bbec4be042b7e
Tony Duan <tony@tonyduan.com>
2019-08-08 11:42:45 +0900

    Improve FGSD embedding.

--- gf/utils.py
+++ gf/utils.py
@@ -81,9 +81,9 @@ def compute_fgsd_embeddings(A, eps=1e-2):
     D = np.diag(np.sum(A, axis=1))
     L = D - A
     W, V = np.linalg.eigh(L)
-    if np.mean(V[:,0]) < 0: # todo: make this less hacky
+    if np.mean(V[:,0]) < 0: # due to numerical imprecision
         V[:,0] = -V[:,0]
-    W[W < eps] = 0.1
+    W[W < eps] = 1 / len(A) # scales with number of nodes
     W[W > eps] = 1 / W[W > eps]
     E = V @ np.diag(np.sqrt(W))
     return np.real(E)
